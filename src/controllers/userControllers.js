e2c79d24b522a8ea9a321f3387892f3586004b77
Leonardo Sampaio <profleonardo95@gmail.com>
2023-03-29 16:53:57 -0300

    feat: copy and paste structure

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,17 @@
+import userServices from "../services/userServices.js";
+
+async function create(req, res) {
+  const { name, email, password } = req.body;
+
+  try {
+    await userServices.create({name, email, password})
+
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+}
+
+export default {
+  create,
+};
